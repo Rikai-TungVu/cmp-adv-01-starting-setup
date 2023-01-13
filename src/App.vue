ddb43158cd8ee889c6834cb12a5332b762967d1d
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-13 18:05:50 +0900

    thay doi thanh local componets

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,19 @@
 
 <script>
 import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 /* Import trực tiếp vào component App*/
 /* Thay vì khai báo hết ở main.js */
 
+
 export default {
   components:{
-    'the-header': TheHeader
+    'the-header': TheHeader,
     /* Định nghĩa component TheHeader */
+    /* Local component */
+    'user-info': UserInfo,
+    'badge-list': BadgeList
   },
   data() {
     return {
